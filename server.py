70239b1318700be5794fd1eac50343c0f4e8eb6a
daniel brayner <danielbrayner93@gmail.com>
2026-01-23 16:52:49 -0300

    Migração do sistema para suportar PostgreSQL no Render2.0

--- server.py
+++ server.py
@@ -595,7 +595,7 @@ def excluir_horarios_lote():
     return redirect("/admin#horarios")
 
 
-if __name__ == '__main__': app.run(host='127.0.0.1', port=5000, debug=True)
+#if __name__ == '__main__': app.run(host='127.0.0.1', port=5000, debug=True)
 
-#if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
